e28e3d2680826fd8e0081286d6551503805d1594
sathish6502 <madevsathishkumar6502@gmail.com>
2026-04-01 09:26:56 +0530

    merging to sathish branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Feature from './Components/Work/Feature'
+import Featurework from './Components/Work/Featurework'
+import GetStarted from './Components/Work/GetStarted'
 
 const App = () => {
   return (
-    <div></div>
+    <div className='min-h-screen bg-[linear-gradient(135deg,#0A0E27_0%,#000000_50%,#0D1B3A_100%)]'>
+          <Feature/>
+          <Featurework/>
+          <GetStarted/>
+    </div>
   )
 }
 
