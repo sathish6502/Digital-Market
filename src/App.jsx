3d15af45338b1e14866e7f13c18e188314aefe4b
rajaduraicbe <rajadurai2496cbe@gmail.com>
2026-04-01 10:14:01 +0530

    appjsx commit changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Individualservice from './Pages/Individualservice'
 
 const App = () => {
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div>
    <BrowserRouter>
    <Routes>
     <Route path="/service" element={<Service />}/>
