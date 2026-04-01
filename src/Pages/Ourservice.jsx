ec3e8ae6ce33cdd4b78313b0ddeb2c3c0df7bc70
gayathri23-y <gayudecember23@gmail.com>
2026-04-01 15:06:04 +0530

    update

---
+++ src/Pages/Ourservice.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Ready from '../Components/Ourservice/Ready'
+import Work from '../Components/Ourservice/Work'
+
+const Ourservice = () => {
+  return (
+    <div>
+      <Ready />
+      <Work />
+    </div>
+  )
+}
+
+export default Ourservice
