99f9d49578635f3051e4dc6dd25fd5dbfbf608eb
sathish6502 <madevsathishkumar6502@gmail.com>
2026-04-02 17:08:40 +0530

    Work page update

--- src/Components/Work/Feature.jsx
+++ src/Components/Work/Feature.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import featuredIcon from "../../assets/Icon.png"; // your icon
-import portfolio from '../../assets/WorkPortfolio.png'
+import featuredIcon from "./../../assets/icon.jpg"; // your icon
+import portfolio from '../../assets/WorkPortfolio.jpg'
 
 const Feature = () => {
   return (
